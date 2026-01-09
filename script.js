fd03c296d2573d8f073461c225fea2c43fb59753
Diaz151017 <diazoliver151017@gmail.com>
2026-01-09 17:36:33 +0100

    Guardado seguro

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-// --- EXERCICI 1.1: Coneixement de colors ---
-// 1. Pedimos el dato y lo guardamos en una variable
+// --- EXERCICI 1: Coneixement de colors i càlcul d’àrea ---
+
+// --- Coneixement de colors ---
+// Pedimos el dato y lo guardamos en una variable
 // .toLowerCase() convierte lo que escriba a minúsculas para evitar errores
 let colorUsuario = prompt("¿Cuál es tu color preferido?").toLowerCase();
 
-// 2. Comprobamos con lógica (IF / ELSE)
+// Comprobamos con lógica (IF / ELSE)
 // || significa "O" (si es verde O es azul O es rojo...)
 if (colorUsuario === "verd" || colorUsuario === "blau" || colorUsuario === "vermell") {
     alert("Color primari");
@@ -11,10 +13,10 @@ if (colorUsuario === "verd" || colorUsuario === "blau" || colorUsuario === "verm
     alert("Altre color");
 }
 
-// --- EXERCICI 1.2: Càlcul d’àrea ---
+// --- Càlcul d’àrea ---
 // El código sigue bajando y ejecutándose...
 
-// Pide las variables. Recuerda: prompt devuelve TEXTO.
+// Pide las variables. Prompt devuelve TEXTO.
 // parseFloat convierte texto a números decimales (por si ponen 5.5)
 let ancho = parseFloat(prompt("Introduce el ancho del rectángulo:"));
 let alto = parseFloat(prompt("Introduce la altura del rectángulo:"));
@@ -25,4 +27,3 @@ if (isNaN(ancho) || isNaN(alto) || ancho <= 0 || alto <= 0) {
     let area = ancho * alto;
     alert("El área del rectángulo es: " + area);
 }
-// Aquí te toca a ti escribir la lógica del cálculo y el IF para validar si son positivos...
